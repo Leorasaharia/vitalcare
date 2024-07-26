a1d571c9f44893bd447f85fdb8c31f3a87a76f39
leorasaharia <saharialeora2406@gmail.com>
2024-07-26 23:39:32 +0530

    files updated

--- src/routes.js
+++ src/routes.js
@@ -12,7 +12,7 @@ import MainDashboard from "views/admin/default";
 // import NFTMarketplace from "views/admin/marketplace";
 import DataTables from "views/admin/dataTables";
 import Profile from "views/admin/profile";
-import RTL from "views/admin/rtl";
+// import RTL from "views/admin/rtl";
 
 // Auth Imports
 import SignInCentered from "views/auth/signIn";
@@ -62,12 +62,19 @@ const routes = [
     component: SignInCentered,
   },
   {
-    name: "RTL Admin",
-    layout: "/rtl",
-    path: "/rtl-default",
-    icon: <Icon as={MdHome} width='20px' height='20px' color='inherit' />,
-    component: RTL,
+    name: "HealthBot",
+    layout: "/admin",
+    path: "/healthbot",
+    icon: <Icon as={MdLock} width='20px' height='20px' color='inherit' />,
+    component: SignInCentered,
   },
+  // {
+  //   name: "RTL Admin",
+  //   layout: "/rtl",
+  //   path: "/rtl-default",
+  //   icon: <Icon as={MdHome} width='20px' height='20px' color='inherit' />,
+  //   component: RTL,
+  // },
 ];
 
 export default routes;
